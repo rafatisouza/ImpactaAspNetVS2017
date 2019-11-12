b20b1e069b8b8f39b8cf59d7007d02d9b086202a
impacta.treinamentos <impacta.treinamentos@hotmail.com>
2019-11-11 23:42:54 -0200

    Aula 11/11/2019

--- AspNet.Capitulo03.Portfolio/Controllers/HomeController.cs
+++ AspNet.Capitulo03.Portfolio/Controllers/HomeController.cs
@@ -1,29 +1,70 @@
-﻿using System;
+﻿using AspNet.Capitulo03.Portfolio.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace AspNet.Capitulo03.Portfolio.Controllers
 {
-    public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
+    public class HomeController : Controller {
+        public ActionResult Index() {
             return View();
         }
 
-        public ActionResult About()
-        {
+        public ActionResult About() {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        [HttpGet]// opcional
+        public ActionResult Contact() {
+            ViewBag.Message = "Deixe sua mensagem";
 
+            return View();
+        }
+        //public ActionResult Contact(string nome, string email, string mensagem) {
+        //--------------------------------------------------------------------
+        //public ActionResult Contact(FormCollection formulario) {
+        //    var nome = formulario["nome"];
+        [HttpPost]
+        public ActionResult Contact(ContatoViewModel contato) {
+            if (!ModelState.IsValid) {
+                return View(contato);
+            }
+
+            var stringConexao = ConfigurationManager.ConnectionStrings["portfolioConnectionString"].ConnectionString;
+            using (var conexao = new SqlConnection(stringConexao)) {
+                conexao.Open();
+                const string instrucao = @"INSERT INTO [dbo].[Contato]
+                                               ([Nome]
+                                               ,[Email]
+                                               ,[Mensagem])
+                                         VALUES
+                                               (@Nome
+                                               ,@Email
+                                               ,@mensagem)";
+                //Todo: não esqueça do meu IDisposable
+                using (var comando = new SqlCommand(instrucao, conexao)) {
+
+                    comando.Parameters.AddWithValue("Nome", contato.Nome);
+                    comando.Parameters.AddWithValue("Email", contato.Email);
+                    comando.Parameters.AddWithValue("mensagem", contato.Mensagem);
+                    comando.ExecuteNonQuery();
+
+                }
+                ModelState.Clear();
+                ViewBag.Sucesso = true;
+                return View();
+            }
+
+        }
+
+        public ActionResult Portfolio() 
+        {
             return View();
         }
     }
---
+++ AspNet.Capitulo03.Portfolio/Models/PortfolioViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNet.Capitulo03.Portfolio.Models {
+    public class PortfolioViewModel {
+        public List<string> CaminhosImagens { get; set; }
+    }
+}
