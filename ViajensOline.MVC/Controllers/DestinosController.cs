7650358daf7dabcdc8557497225e1c5909bce87f
impacta.treinamentos <impacta.treinamentos@hotmail.com>
2019-11-14 23:36:33 -0200

    Aula 14/11/2019

--- ViajensOline.MVC/Controllers/DestinosController.cs
+++ ViajensOline.MVC/Controllers/DestinosController.cs
@@ -1,24 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using ViagemOnline.Dominio;
 using ViagensOnline.Repositorios.SqlServel;
+using ViajensOline.MVC.Models;
 
 namespace ViajensOline.MVC.Controllers
 {
     public class DestinosController : Controller
     {
         private ViagensOnlineDbContext db = new ViagensOnlineDbContext();
+        private string path;
+        private string caminhoImagensDestinos = ConfigurationManager.AppSettings["caminhoImagensDestinos"];
 
         // GET: Destinos
         public ActionResult Index()
         {
-            return View(db.Destinos.ToList());
+            return View(Mapear(db.Destinos.ToList()));
+        }
+
+        private List<DestinoViewModel> Mapear(List<Destino> destinos)
+        {
+            List <DestinoViewModel> ViewModels = new List<DestinoViewModel>();
+
+            foreach (var destino in destinos)
+            {
+
+                ViewModels.Add(Mapear(destino)); 
+                    
+
+            }
+
+            return ViewModels;
+
+
+        }
+
+        private DestinoViewModel Mapear(Destino destino)
+        {
+            DestinoViewModel retorno = new DestinoViewModel() {
+                CaminhoImagem = Path.Combine(caminhoImagensDestinos, destino.NomeImagem),
+                Cidade = destino.Cidade,
+                Id = destino.Id,
+                Nome = destino.NomeImagem,
+                Pais = destino.Pais                
+            };
+            return retorno;
+            
         }
 
         // GET: Destinos/Details/5
@@ -33,7 +68,7 @@ namespace ViajensOline.MVC.Controllers
             {
                 return HttpNotFound();
             }
-            return View(destino);
+            return View(Mapear(destino));
         }
 
         // GET: Destinos/Create
@@ -47,16 +82,41 @@ namespace ViajensOline.MVC.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Nome,Pais,Cidade,NomeImagem")] Destino destino)
+        public ActionResult Create(DestinoViewModel ViewModel)
         {
+                if (ViewModel.ArquivoForto == null)
+                {
+                    ModelState.AddModelError("","Ops: selecione a imagem");
+                }
             if (ModelState.IsValid)
             {
-                db.Destinos.Add(destino);
+                SalvarFoto(ViewModel.ArquivoForto);
+                db.Destinos.Add(Mapear(ViewModel));
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(destino);
+            return View(ViewModel);
+        }
+
+        private void SalvarFoto(HttpPostedFileBase arquivoForto)
+        {
+            var caminhoVirtual = Path.Combine(caminhoImagensDestinos, arquivoForto.FileName);
+            arquivoForto.SaveAs(Request.MapPath(caminhoVirtual));
+        }
+
+        private Destino Mapear(DestinoViewModel viewModel)
+        {
+            Destino destino = new Destino() {
+                   Cidade = viewModel.Cidade,
+                   Id = viewModel.Id = viewModel.Id,
+                   Nome = viewModel.Nome,
+                   NomeImagem = viewModel.ArquivoForto.FileName,
+                   Pais = viewModel.Pais
+            };
+
+
+            return destino;
         }
 
         // GET: Destinos/Edit/5
@@ -71,7 +131,7 @@ namespace ViajensOline.MVC.Controllers
             {
                 return HttpNotFound();
             }
-            return View(destino);
+            return View(Mapear(destino));
         }
 
         // POST: Destinos/Edit/5
@@ -79,15 +139,23 @@ namespace ViajensOline.MVC.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Nome,Pais,Cidade,NomeImagem")] Destino destino)
+        public ActionResult Edit(DestinoViewModel viewModel)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(destino).State = EntityState.Modified;
+                var destino = db.Destinos.Find(viewModel.Id);
+                
+                db.Entry(destino).CurrentValues.SetValues(viewModel);
+                //db.Entry(viewModel).State = EntityState.Modified;
+                if (viewModel.ArquivoForto != null)
+                {
+                    SalvarFoto(viewModel.ArquivoForto);
+                    destino.NomeImagem = viewModel.ArquivoForto.FileName;
+                }
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(destino);
+            return View(viewModel);
         }
 
         // GET: Destinos/Delete/5
@@ -102,7 +170,7 @@ namespace ViajensOline.MVC.Controllers
             {
                 return HttpNotFound();
             }
-            return View(destino);
+            return View(Mapear(destino));
         }
 
         // POST: Destinos/Delete/5
